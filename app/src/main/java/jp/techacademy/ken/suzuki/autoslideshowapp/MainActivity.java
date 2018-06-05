5accc9c5a4258150754f715e0b7e35a065aa9efe
鈴木顕 <yunalovely1977@yahoo.co.jp>
2018-06-06 03:31:07 +0900

    4回目のコミット

--- app/src/main/java/jp/techacademy/ken/suzuki/autoslideshowapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/ken/suzuki/autoslideshowapp/MainActivity.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -36,90 +37,103 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mStartButton = (Button) findViewById(R.id.start_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
 
         // Android 6.0以降の場合
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // パーミッションの許可状態を確認する
             if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
                 // 許可されている
+                // getContentsInfoメソッドを読み込む
                 getContentsInfo();
             } else {
                 // 許可されていないので許可ダイアログを表示する
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+                // getContentsInfoメソッドを読み込まないしてください
             }
             // Android 5系以下の場合
         } else {
+            // getContentsInfoメソッドを読み込む
             getContentsInfo();
         }
-
-        mPrevButton = (Button) findViewById(R.id.prev_button);
-        mStartButton = (Button) findViewById(R.id.start_button);
-        mNextButton = (Button) findViewById(R.id.next_button);
-
 
         // 再生ボタンの処理
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                // cursorの中身（DBのレコード）が0の場合エラーを起こすので、中身を判定する
+                if ((cursor != null) && cursor.getCount() > 0) {
 
 
-                if (mTimer == null) {
+                    if (mTimer == null) {
 
-                    // 進む戻るボタンのタップ不可
+                        // 進む戻るボタンのタップ不可
+                        mPrevButton.setEnabled(false);
+                        mNextButton.setEnabled(false);
+
+                        // ボタンの表示を停止に変更
+                        mStartButton.setText(String.format("停止"));
+
+                        mTimer = new Timer();
+                        mTimer.schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                mTimerSec += 0.1;
+
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+
+                                        // 自動的に次の画像に切り替えるので moveToFirst
+                                        if (cursor.moveToNext()) {
+                                            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                                            Long id = cursor.getLong(fieldIndex);
+                                            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                                            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                                            imageView.setImageURI(imageUri);
+                                        } else {
+                                            // 最初の画面を取得する
+                                            cursor.moveToFirst();
+                                            int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                                            Long id = cursor.getLong(fieldIndex);
+                                            Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                                            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                                            imageView.setImageURI(imageUri);
+                                        }
+                                    }
+                                });
+                            }
+                        }, 2000, 2000);
+                    } else {
+                        // 自動送りを停止する処理
+                        mTimer.cancel();
+                        mTimer = null;
+
+                        // 進む戻るボタンをタップ可に変更
+                        mPrevButton.setEnabled(true);
+                        mNextButton.setEnabled(true);
+
+                        // ボタンの表示を再生に変更
+                        mStartButton.setText(String.format("再生"));
+                    }
+                } else {
+                    // ボタンをタップ不可に変更
                     mPrevButton.setEnabled(false);
                     mNextButton.setEnabled(false);
+                    mStartButton.setEnabled(false);
 
-                    // ボタンの表示を停止に変更
-                    mStartButton.setText(String.format("停止"));
-
-                    mTimer = new Timer();
-                    mTimer.schedule(new TimerTask() {
-                        @Override
-                        public void run() {
-                            mTimerSec += 0.1;
-
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-
-                                    // 自動的に次の画像に切り替えるので moveToFirst
-                                    if (cursor.moveToNext()) {
-                                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                                        Long id = cursor.getLong(fieldIndex);
-                                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-                                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                                        imageView.setImageURI(imageUri);
-                                    }
-                                    else {
-                                        // 最初の画面を取得する
-                                        cursor.moveToFirst();
-                                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                                        Long id = cursor.getLong(fieldIndex);
-                                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
-
-                                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                                        imageView.setImageURI(imageUri);
-                                    }
-                                }
-                            });
-                        }
-                    }, 2000, 2000);
-                }
-                else{
-                    // 自動送りを停止する処理
-                    mTimer.cancel();
-                    mTimer = null;
-
-                    // 進む戻るボタンをタップ可に変更
-                    mPrevButton.setEnabled(true);
-                    mNextButton.setEnabled(true);
-
-                    // ボタンの表示を再生に変更
-                    mStartButton.setText(String.format("再生"));
+                    // Toastを追加
+                    Toast toast = Toast.makeText(MainActivity.this, "表示できません", Toast.LENGTH_LONG);
+                    toast.show();
                 }
             }
         });
@@ -128,23 +142,37 @@ public class MainActivity extends AppCompatActivity {
         mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cursor.moveToPrevious()) {
-                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                    Long id = cursor.getLong(fieldIndex);
-                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                    imageView.setImageURI(imageUri);
-                }
-                else {
-                    // 最後の画面を取得する
-                    cursor.moveToLast();
-                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                    Long id = cursor.getLong(fieldIndex);
-                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                // cursorの中身（DBのレコード）が0の場合エラーを起こすので、中身を判定する
+                if ((cursor != null) && cursor.getCount() > 0) {
 
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                    imageView.setImageURI(imageUri);
+
+                    if (cursor.moveToPrevious()) {
+                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                        Long id = cursor.getLong(fieldIndex);
+                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                        imageView.setImageURI(imageUri);
+                    } else {
+                        // 最後の画面を取得する
+                        cursor.moveToLast();
+                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                        Long id = cursor.getLong(fieldIndex);
+                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                        imageView.setImageURI(imageUri);
+                    }
+                } else {
+                    // ボタンをタップ不可に変更
+                    mPrevButton.setEnabled(false);
+                    mNextButton.setEnabled(false);
+                    mStartButton.setEnabled(false);
+
+                    // Toastを追加
+                    Toast toast = Toast.makeText(MainActivity.this, "表示できません", Toast.LENGTH_LONG);
+                    toast.show();
                 }
             }
         });
@@ -153,34 +181,76 @@ public class MainActivity extends AppCompatActivity {
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (cursor.moveToNext()) {
-                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                    Long id = cursor.getLong(fieldIndex);
-                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
 
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                    imageView.setImageURI(imageUri);
-                }
-                else {
-                    // 最初の画面を取得する
-                    cursor.moveToFirst();
-                    int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
-                    Long id = cursor.getLong(fieldIndex);
-                    Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+                // cursorの中身（DBのレコード）が0の場合エラーを起こすので、中身を判定する
+                if (( cursor != null ) && cursor.getCount() > 0 ) {
 
-                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
-                    imageView.setImageURI(imageUri);
+                    if (cursor.moveToNext()) {
+
+                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                        Long id = cursor.getLong(fieldIndex);
+                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                        imageView.setImageURI(imageUri);
+                    } else {
+                        // 最初の画面を取得する
+                        cursor.moveToFirst();
+                        int fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID);
+                        Long id = cursor.getLong(fieldIndex);
+                        Uri imageUri = ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id);
+
+                        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                        imageView.setImageURI(imageUri);
+                    }
+                } else {
+                    // ボタンをタップ不可に変更
+                    mPrevButton.setEnabled(false);
+                    mNextButton.setEnabled(false);
+                    mStartButton.setEnabled(false);
+
+                    // Toastを追加
+                    Toast toast = Toast.makeText(MainActivity.this, "表示できません", Toast.LENGTH_LONG);
+                    toast.show();
                 }
             }
         });
     }
 
+    // ユーザの許可、不許可の選択結果を受け取って処理する
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case PERMISSIONS_REQUEST_CODE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("ANDROID", "許可された");
+                    // ここでgetContentsInfoメソッドを読み込むのがポイント
                     getContentsInfo();
+
+                    // Toastを追加
+                    Toast toast = Toast.makeText(MainActivity.this, "許可されました", Toast.LENGTH_LONG);
+                    toast.show();
+                } else {
+                    Log.d("ANDROID", "許可されなかった");
+
+                    //ユーザーから許可されるまでダイアログを表示する処理
+                    // Android 6.0以降の場合
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        // パーミッションの許可状態を確認する
+                        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                            // 許可されている
+                            // getContentsInfoメソッドを読み込む
+                            getContentsInfo();
+                        } else {
+                            // 許可されていないので許可ダイアログを表示する
+                            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_REQUEST_CODE);
+                            // getContentsInfoメソッドを読み込まない
+                        }
+                        // Android 5系以下の場合
+                    } else {
+                        // getContentsInfoメソッドを読み込む
+                        getContentsInfo();
+                    }
                 }
                 break;
             default:
@@ -217,7 +287,6 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         if (cursor != null) {
-
             cursor.close();
         }
     }
